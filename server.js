7ad3ca1d390261a0b0aaaec7fee22a4927065f09
Joao Marques <joaovitormarquesribeiro@outlook.com>
2023-10-04 15:57:03 -0400

    Setup project

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+let path = require('path');
+const router = require('./routes');
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
+
+app.use(router)
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
